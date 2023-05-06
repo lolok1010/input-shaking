fcd7a53c79b86eb16e68f308b1ac1ccb6fd608fe
vako lekiashvili <124619199+lolok1010@users.noreply.github.com>
2023-05-06 15:39:01 +0400

    Add files via upload

---
+++ javascr/app.js
@@ -0,0 +1,95 @@
+
+
+
+// 1)	Შექმენით ფუნქცია რომელსაც პარამეტრების სახით გადაეცემა 2 მნიშნელობა ,
+//  მასივი რომელშიც ჩაყრილია რენდომული რიცხვები 0 დან 100 მდე შალედში
+//   მოთავსებულიდა მეორე მასივი რომელშიც ჩაყრილია შემდეგი რიცხვები
+//    [ 2,3,5,11] დააბრუნეთ ამ ფუნქციიდან ყველა ის რიცხვი პირველი 
+//    მასივიდან რომელიც იყოფა მეორე მასივის ერთ წევრზე მაინც უნაშთოდ
+
+
+// let array = [100,230,60,57,15,18];
+// let list =  [2,3,5,11];
+// console.log(list)
+// function fillArray(arr){
+
+//     for(let i = 0; i < 10; i++){
+//         let randNumber = Math.round(Math.random() * 100);
+//         arr.push(randNumber);
+//     }
+
+// }
+// fillArray(array);
+// console.log(array);
+
+// function isDevidible(array,arr){
+
+//   for(let i = 0; i < array.length; i++){
+//     // console.log(array[i]);
+
+//     for(let j = 0; j < arr.length; j++){
+//         if(array[i] % arr[j] == 0){
+//             console.log(array[i]);
+//             break;
+//         }
+//     }
+//   }
+// }
+// isDevidible(array,list);
+
+
+
+// შექმენით ფუნქცია რომელსაც გადასცემთ რიცხვების მასივს პარამეტრის სახით
+// ფუნქციამ უნდა დააბრუნოს ახალი შეტრიალებული მასივი
+// [1,17,50]
+// [50,17,1]
+
+// შექმენით ფუნქცია რომელსაც პარამეტრის სახით გადასცემთ სტრინგს
+// და ფუნქციამ უნდა შეაბრუნოს სტრინგი რომელსაც დაბეჭდავთ კონსოლში
+// html
+// lmth
+
+// alert("warning")
+
+// let text = "html"
+
+
+
+
+// let list = [20,50,70];
+// let tmp = [];
+// function reverseArray(array,tmp){
+ 
+//     for(let i = array.length - 1; i >= 0; i--){
+
+//         // console.log(array[i]);
+//         tmp.push(array[i])
+
+//     }
+//     console.log(tmp)
+
+
+// }
+// reverseArray(list,tmp);
+
+// let text = "html";
+// let txt = "javascript"
+// console.log(txt[1])
+
+// function reverseStr(str){
+   
+//     let reversedString = '';
+
+//     for(let i = str.length - 1 ; i >= 0; i--){
+
+       
+//         reversedString += str[i]
+
+//     }
+
+//   console.log(reversedString);
+// }
+// reverseStr(text);
+// reverseStr(txt);
+
+
